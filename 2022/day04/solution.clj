bc05bd6cc96445c9d47aa98d7139971f6688dbaf
Eugene Dementjev <eugene.dementjev@lokalise.com>
2022-12-04 13:27:54 +0400

    refactor: defn naming

--- 2022/day04/solution.clj
+++ 2022/day04/solution.clj
@@ -4,30 +4,30 @@
 (defn to-int-range [str]
   (map #(Integer/parseInt %) (str/split str #"-")))
 
-(defn in-range [num range]
+(defn in-range? [num range]
   (<= (first range) num (last range)))
 
-(defn is-subset [rangeA rangeB]
-  (and (in-range (first rangeA) rangeB) (in-range (last rangeA) rangeB)))
+(defn subset? [rangeA rangeB]
+  (and (in-range? (first rangeA) rangeB) (in-range? (last rangeA) rangeB)))
 
-(defn overlap [rangeA rangeB]
-  (or (in-range (first rangeA) rangeB) (in-range (last rangeA) rangeB)))
+(defn overlaps? [rangeA rangeB]
+  (or (in-range? (first rangeA) rangeB) (in-range? (last rangeA) rangeB)))
 
-(defn is-subset-pair [rangeA rangeB]
-  (or (is-subset rangeA rangeB) (is-subset rangeB rangeA)))
+(defn subset-pair? [rangeA rangeB]
+  (or (subset? rangeA rangeB) (subset? rangeB rangeA)))
 
-(defn is-overlap-pair [rangeA rangeB]
-  (or (overlap rangeA rangeB) (overlap rangeB rangeA)))
+(defn overlap-pair? [rangeA rangeB]
+  (or (overlaps? rangeA rangeB) (overlaps? rangeB rangeA)))
 
 (defn part-one [input]
   (count
    (filter true?
-           (map #(apply is-subset-pair (map to-int-range (str/split % #","))) input))))
+           (map #(apply subset-pair? (map to-int-range (str/split % #","))) input))))
 
 (defn part-two [input]
   (count
    (filter true?
-           (map #(apply is-overlap-pair (map to-int-range (str/split % #","))) input))))
+           (map #(apply overlap-pair? (map to-int-range (str/split % #","))) input))))
 
 (defn Main []
   (let [input (str/split-lines (slurp "input.txt"))]
