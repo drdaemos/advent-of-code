7876da5c3453e96becbc1bce7430a023c3073699
Eugene Dementjev <eugene.dementjev@lokalise.com>
2022-12-04 13:22:23 +0400

    refactor: day 4 clojure style

--- 2022/day04/solution.clj
+++ 2022/day04/solution.clj
@@ -1,47 +1,36 @@
-(ns day04)
+(ns solution
+  (:require [clojure.string :as str]))
 
-(defn toIntRange [str]
-    (map #(Integer/parseInt %) (clojure.string/split str #"-"))
-)
+(defn to-int-range [str]
+  (map #(Integer/parseInt %) (str/split str #"-")))
 
-(defn inRange [num range]
-    (and (>= num (first range)) (<= num (last range)))
-)
+(defn in-range [num range]
+  (<= (first range) num (last range)))
 
-(defn isSubset [rangeA rangeB]
-    (and (inRange (first rangeA) rangeB) (inRange (last rangeA) rangeB))
-)
+(defn is-subset [rangeA rangeB]
+  (and (in-range (first rangeA) rangeB) (in-range (last rangeA) rangeB)))
 
 (defn overlap [rangeA rangeB]
-    (or (inRange (first rangeA) rangeB) (inRange (last rangeA) rangeB))
-)
+  (or (in-range (first rangeA) rangeB) (in-range (last rangeA) rangeB)))
 
-(defn isSubsetPair [a b]
-    (def rangeA (toIntRange a))
-    (def rangeB (toIntRange b))
-    (or (isSubset rangeA rangeB) (isSubset rangeB rangeA))
-)
+(defn is-subset-pair [rangeA rangeB]
+  (or (is-subset rangeA rangeB) (is-subset rangeB rangeA)))
 
-(defn isOverlapPair [a b]
-    (def rangeA (toIntRange a))
-    (def rangeB (toIntRange b))
-    (or (overlap rangeA rangeB) (overlap rangeB rangeA))
-)
+(defn is-overlap-pair [rangeA rangeB]
+  (or (overlap rangeA rangeB) (overlap rangeB rangeA)))
 
-(defn partOne [input]
-    (count 
-        (filter true? 
-            (map #(apply isSubsetPair (clojure.string/split % #",")) input)))
-)
+(defn part-one [input]
+  (count
+   (filter true?
+           (map #(apply is-subset-pair (map to-int-range (str/split % #","))) input))))
 
-(defn partTwo [input]
-    (count 
-        (filter true? 
-            (map #(apply isOverlapPair (clojure.string/split % #",")) input)))
-)
+(defn part-two [input]
+  (count
+   (filter true?
+           (map #(apply is-overlap-pair (map to-int-range (str/split % #","))) input))))
 
 (defn Main []
-    (def input (clojure.string/split-lines (slurp "input.txt")))
-    (println "Part one:" (partOne input)) ;; 582
-    (println "Part two:" (partTwo input)) ;; 582
-)(Main)
+  (let [input (str/split-lines (slurp "input.txt"))]
+    (println "Part one:" (part-one input)) ;; 582
+    (println "Part two:" (part-two input)) ;; 893
+    ))(Main)
