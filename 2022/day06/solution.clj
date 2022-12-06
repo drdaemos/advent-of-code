a0a5e4c1624e7e564f08be0e635a8796015d24d3
Eugene Dementjev <eugene.dementjev@lokalise.com>
2022-12-06 11:30:13 +0400

    feat: 2022 day 6 solution

---
+++ 2022/day06/solution.clj
@@ -0,0 +1,18 @@
+(ns solution)
+
+(defn marker? [seq]
+  (apply distinct? seq))
+
+(defn start-of [input len]
+  (loop [i len
+         buf input]
+    (if (marker? (take len buf))
+      i
+      (recur (inc i) (drop 1 buf)))))
+
+(defn Main []
+  (let
+   [input (-> "input.txt" slurp)]
+    (println "Part one:" (start-of input 4)) ;; 1848
+    (println "Part two:" (start-of input 14)) ;; 2308
+    ))(Main)
