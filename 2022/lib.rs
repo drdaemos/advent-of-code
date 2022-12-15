de7d613d636466841763bc8cef8a2457408d73e0
Eugene Dementjev <eugene.dementjev@lokalise.com>
2022-12-15 18:09:09 +0400

    chore: rust boilerplate

---
+++ 2022/lib.rs
@@ -0,0 +1,16 @@
+use std::fs;
+pub mod utils {
+    use super::*;
+    pub fn get_file_contents(file_path: &str) -> String {
+        fs::read_to_string(file_path)
+            .unwrap()
+            .replace("\r\n", "\n")
+            .replace("\r", "\n")
+    }
+    pub fn get_lines(file_path: &str) -> Vec<String> {
+        get_file_contents(file_path)
+            .lines()
+            .map(ToOwned::to_owned)
+            .collect()
+    }
+}
---
+++ 2022/main.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input: &str = &args[1];
+    match input {
+        &_ => todo!()
+    }
+}
